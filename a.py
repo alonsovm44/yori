d3207d787da90dc539508f4862656567dc98f42d
Alonso <alvemo44@gmail.com>
2026-01-23 03:02:33 -0700

    tryna fix universal linkage

---
+++ a.py
@@ -0,0 +1,6 @@
+def sumar_e_imprimir(a, b):
+    print(f"La suma es: {a + b}")
+    print("hello from python")
+
+# Llamada a la función con 3 y 2
+sumar_e_imprimir(3, 2)
---
+++ c.ts
@@ -0,0 +1,7 @@
+const imprimirAleatorio = (): void => {
+    const numero: number = Math.random();
+    console.log(`Número aleatorio: ${numero}`);
+    console.log("hello from typescript");
+};
+
+imprimirAleatorio();
